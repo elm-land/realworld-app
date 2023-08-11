731dfedd41d56a022df1b945496ae920a37441c4
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2023-08-11 14:09:08 -0500

    Upgrade Auth module

--- src/Auth.elm
+++ src/Auth.elm
@@ -1,21 +1,31 @@
-module Auth exposing (User, beforeProtectedInit)
+module Auth exposing (User, onPageLoad, viewLoadingPage)
 
 import Api.User
+import Dict exposing (Dict)
 import ElmSpa.Page as ElmSpa
-import Gen.Route exposing (Route)
-import Request exposing (Request)
+import Route exposing (Route)
 import Shared
+import View exposing (View)
 
 
 type alias User =
     Api.User.User
 
 
-beforeProtectedInit : Shared.Model -> Request -> ElmSpa.Protected User Route
-beforeProtectedInit shared req =
+onPageLoad : Shared.Model -> Route () -> Auth.Action.Action User
+onPageLoad shared req =
     case shared.user of
         Just user ->
-            ElmSpa.Provide user
+            Auth.Action.loadPageWithUser user
 
         Nothing ->
-            ElmSpa.RedirectTo Gen.Route.Login
+            Auth.Action.replaceRoute
+                { path = Route.Path.Login
+                , query = Dict.empty
+                , hash = Nothing
+                }
+
+
+viewLoadingPage : Shared.Model -> Route () -> View Never
+viewLoadingPage shared route =
+    View.none
