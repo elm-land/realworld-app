9f953985ae7768ccc7c9a33c3a53671f61707aa5
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2023-08-11 14:50:26 -0500

    Update component imports

--- src/Components/Navbar.elm
+++ src/Components/Navbar.elm
@@ -1,30 +1,30 @@
 module Components.Navbar exposing (view)
 
 import Api.User exposing (User)
-import Gen.Route as Route exposing (Route)
 import Html exposing (..)
 import Html.Attributes exposing (class, classList, href)
 import Html.Events as Events
+import Route.Path exposing (Path)
 
 
 view :
     { user : Maybe User
-    , currentRoute : Route
+    , currentRoutePath : Path
     , onSignOut : msg
     }
     -> Html msg
 view options =
     nav [ class "navbar navbar-light" ]
         [ div [ class "container" ]
-            [ a [ class "navbar-brand", href (Route.toHref Route.Home_) ] [ text "conduit" ]
+            [ a [ class "navbar-brand", Route.Path.href Route.Path.Home_ ] [ text "conduit" ]
             , ul [ class "nav navbar-nav pull-xs-right" ] <|
                 case options.user of
                     Just _ ->
                         List.concat
-                            [ List.map (viewLink options.currentRoute) <|
-                                [ ( "Home", Route.Home_ )
-                                , ( "New Article", Route.Editor )
-                                , ( "Settings", Route.Settings )
+                            [ List.map (viewLink options.currentRoutePath) <|
+                                [ ( "Home", Route.Path.Home_ )
+                                , ( "New Article", Route.Path.Editor )
+                                , ( "Settings", Route.Path.Settings )
                                 ]
                             , [ li [ class "nav-item" ]
                                     [ a
@@ -37,22 +37,22 @@ view options =
                             ]
 
                     Nothing ->
-                        List.map (viewLink options.currentRoute) <|
-                            [ ( "Home", Route.Home_ )
-                            , ( "Sign in", Route.Login )
-                            , ( "Sign up", Route.Register )
+                        List.map (viewLink options.currentRoutePath) <|
+                            [ ( "Home", Route.Path.Home_ )
+                            , ( "Sign in", Route.Path.Login )
+                            , ( "Sign up", Route.Path.Register )
                             ]
             ]
         ]
 
 
-viewLink : Route -> ( String, Route ) -> Html msg
-viewLink currentRoute ( label, route ) =
+viewLink : Path -> ( String, Path ) -> Html msg
+viewLink currentRoutePath ( label, routePath ) =
     li [ class "nav-item" ]
         [ a
             [ class "nav-link"
-            , classList [ ( "active", currentRoute == route ) ]
-            , href (Route.toHref route)
+            , classList [ ( "active", currentRoutePath == routePath ) ]
+            , Route.Path.href routePath
             ]
             [ text label ]
         ]
--- src/Components/UserForm.elm
+++ src/Components/UserForm.elm
@@ -3,10 +3,10 @@ module Components.UserForm exposing (Field, view)
 import Api.Data exposing (Data)
 import Api.User exposing (User)
 import Components.ErrorList
-import Gen.Route as Route exposing (Route)
 import Html exposing (..)
 import Html.Attributes exposing (class, href, placeholder, type_, value)
 import Html.Events as Events
+import Route.Path exposing (Path)
 
 
 type alias Field msg =
@@ -21,7 +21,7 @@ view :
     { user : Data User
     , label : String
     , onFormSubmit : msg
-    , alternateLink : { label : String, route : Route }
+    , alternateLink : { label : String, route : Path }
     , fields : List (Field msg)
     }
     -> Html msg
@@ -32,7 +32,7 @@ view options =
                 [ div [ class "col-md-6 offset-md-3 col-xs-12" ]
                     [ h1 [ class "text-xs-center" ] [ text options.label ]
                     , p [ class "text-xs-center" ]
-                        [ a [ href (Route.toHref options.alternateLink.route) ]
+                        [ a [ Route.Path.href options.alternateLink.route ]
                             [ text options.alternateLink.label ]
                         ]
                     , case options.user of
